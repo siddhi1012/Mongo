2ebb28ab3725e598c14f7966e6124d92bc85537f
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-24 23:28:15 +0530

    updated files

---
+++ index.js
@@ -0,0 +1,16 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().then(() =>{
+    console.log("Connection Successful...");
+})
+.catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    age : Number,
+});
